f537766476a132fe9d342b925afbc6cace39ea5e
alex <alex@cloudware.it>
2013-09-16 17:40:34 +0200

    Reflect name change in the tests

--- test/test_receipt.rb
+++ test/test_receipt.rb
@@ -4,30 +4,30 @@ require 'salestaxcalc'
 require 'stringio'
 
 
-class CalcTest < Test::Unit::TestCase
-  def new_calc(*args)
+class ReceiptTest < Test::Unit::TestCase
+  def new_receipt(*args)
     SalesTaxCalc::Receipt.new(*args)
   end
 
   def test_create
-    c = new_calc
+    c = new_receipt
     assert_equal SalesTaxCalc::SALES_TAX, c.sales_tax
     assert_equal SalesTaxCalc::IMPORT_TAX, c.import_tax
     assert_equal [], c.items
 
-    c = new_calc(15, 0)
+    c = new_receipt(15, 0)
     assert_equal 15, c.sales_tax
     assert_equal 0, c.import_tax
   end
 
   def test_add
-    c = new_calc
+    c = new_receipt
     c.add('music CD', 14.99, 1, true, false)
     assert_equal 1, c.items.size
   end
 
   def test_totals
-    c = new_calc(10, 5)
+    c = new_receipt(10, 5)
     c.add('music CD', 15, 1, true, false)
     c.add('imported book', 13, 2, false, true)
     assert_equal 1.5 + 26 * 0.05, c.taxes
@@ -48,7 +48,7 @@ class CalcTest < Test::Unit::TestCase
     Total: 29.83
     EOO
     out = StringIO.new
-    new_calc(10, 5).parse(input).print(out)
+    new_receipt(10, 5).parse(input).print(out)
     assert_equal expected.lines.map(&:strip).join("\n"), out.string.strip
   end
 
@@ -64,7 +64,7 @@ class CalcTest < Test::Unit::TestCase
     Total: 65.15
     EOO
     out = StringIO.new
-    new_calc(10, 5).parse(input).print(out)
+    new_receipt(10, 5).parse(input).print(out)
     assert_equal expected.lines.map(&:strip).join("\n"), out.string.strip
   end
 
@@ -84,7 +84,7 @@ class CalcTest < Test::Unit::TestCase
     Total: 74.63
     EOO
     out = StringIO.new
-    new_calc(10, 5).parse(input).print(out)
+    new_receipt(10, 5).parse(input).print(out)
     assert_equal expected.lines.map(&:strip).join("\n"), out.string.strip
   end
 end
