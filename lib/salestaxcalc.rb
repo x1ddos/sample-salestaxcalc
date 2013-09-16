4535c81de7e660672f1cefbf85ce61d8f5dda1c5
alex <alex@cloudware.it>
2013-09-16 13:54:55 +0200

    Some docstrings

--- lib/salestaxcalc.rb
+++ lib/salestaxcalc.rb
@@ -1,6 +1,7 @@
 module SalesTaxCalc
   VERSION = "0.0.1"
 
+  # Default tax rates. Supply yours at Calc initialization.
   SALES_TAX = 10  # 10% standard sales tax
   IMPORT_TAX = 5  # 5% for imported products
 
@@ -15,12 +16,15 @@ module SalesTaxCalc
     (num * (1/precision)).round * precision
   end
 
+  # A single item of a basket.
   class Item
     attr_reader :name, :price, :qty, :tax
 
     # RegExp to match an input line
+    # [_, qty, name, price]
     RE = /^(\d+)\s+(.+)\sat\s([0-9]+(\.[0-9]+)?)/i
     
+    # Specify tax in percentage, e.g. 10, not 0.1
     def initialize(name, price, qty, tax)
       @name = name
       @price = price.to_f
@@ -28,6 +32,11 @@ module SalesTaxCalc
       @tax = tax.to_f
     end
 
+    # Parses a text line and returns Item instance.
+    # Text should conform to RE regexp, e.g.
+    # <qty> something [imported] worth buying at <price>
+    # The "imported" keyword will be placed at the beginning of the name,
+    # if found.
     def self.parse(line, stax, itax)
       m = RE.match(line.to_s.strip)
       name = m[2].to_s
@@ -39,14 +48,17 @@ module SalesTaxCalc
       new(name, m[3].to_f, m[1].to_i, tax)
     end
 
+    # Returns the total amount of taxes 
     def taxes
       @taxes ||= SalesTaxCalc.round(tax * total / 100.0)
     end
 
+    # Total amount, w/o taxes.
     def total
       @total ||= price * qty
     end
 
+    # Total amount after applying taxes.
     def taxed
       @taxed ||= total + taxes
     end
@@ -61,6 +73,9 @@ module SalesTaxCalc
       @items = []
     end
 
+    # Parses a text, possible multiline.
+    # See Item#parse for details
+    # Returns self for chaining.
     def parse(text)
       parsed = text.lines.map { |line|
         Item.parse(line.chomp, sales_tax, import_tax)
@@ -69,24 +84,32 @@ module SalesTaxCalc
       self
     end
     
+    # Add a "row" item.
+    # Taxable and imported are booleans.
+    # Useful for testing?
     def add(name, price, qty, taxable, imported)
       tax = taxable ? sales_tax : 0
       tax += import_tax if imported
       @items << Item.new(name, price, qty, tax)
     end
 
+    # Total amount of taxes.
     def taxes
       sum(:taxes)
     end
 
+    # Total amount of the basket, w/o taxes
     def total
       sum(:total)
     end
 
+    # Total amount with taxes.
     def taxed
       sum(:taxed)
     end
 
+    # Simple basket info print to the supplied output.
+    # Out can be anything, e.g. $stdout or StringIO.new.
     def print(out)
       items.each do |item|
         out.printf("%d %s: %.2f\n", item.qty, item.name, item.taxed)
